6b46b3c35b31d73869d69b1eb3340e5bc972a1e7
Moises Hernandez Fernandez <pemoi1982@gmail.com>
2019-07-15 03:10:34 +0100

    Update with new shfl instructions

--- functions_gpu.h
+++ functions_gpu.h
@@ -2,6 +2,7 @@
 //http://docs.nvidia.com/cuda/cuda-c-programming-guide/#mathematical-functions-appendix
 
 #define FUNC __device__ inline
+#define FULL_MASK 0xFFFFFFFF
 
 FUNC float log_gpu(float x){return logf(x);}
 FUNC double log_gpu(double x){return log(x);}
@@ -45,33 +46,33 @@ FUNC double pow_gpu(double x1, double x2){return pow(x1,x2);}
 FUNC double shfl_down(double x, uint s){
   int lo, hi;
   asm volatile( "mov.b64 {%0,%1}, %2;" : "=r"(lo), "=r"(hi) : "d"(x) );
-  lo = __shfl_down(lo,s);
-  hi = __shfl_down(hi,s);
+  lo = __shfl_down_sync(FULL_MASK,lo,s);
+  hi = __shfl_down_sync(FULL_MASK,hi,s);
   asm volatile( "mov.b64 %0, {%1,%2};" : "=d"(x) : "r"(lo), "r"(hi) );
   return x;
 }
 
 // shfl_down function for single precision
 FUNC float shfl_down(float x, uint s){
-  return __shfl_down(x,s);
+  return __shfl_down_sync(FULL_MASK,x,s);
 } 
 
 // shfl function for double precision
 FUNC double shfl(double x, int s) {
   int lo, hi;
   asm volatile( "mov.b64 {%0,%1}, %2;" : "=r"(lo), "=r"(hi) : "d"(x) );
-  lo = __shfl(lo,s);
-  hi = __shfl(hi,s);
+  lo = __shfl_sync(FULL_MASK,lo,s);
+  hi = __shfl_sync(FULL_MASK,hi,s);
   asm volatile( "mov.b64 %0, {%1,%2};" : "=d"(x) : "r"(lo), "r"(hi) );
   return x;
 }
 
 // shfl function for single precision
 FUNC float shfl(float x, uint s){
-  return __shfl(x,s);
+  return __shfl_sync(FULL_MASK,x,s);
 } 
 
 // shfl function for int
 FUNC int shfl(int x, int s){
-  return __shfl(x,s);
+  return __shfl_sync(FULL_MASK,x,s);
 } 
